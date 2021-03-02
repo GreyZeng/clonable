bd727a115f9867764a7638cfcf80565081b528b0
zenghui <410486047@qq.com>
2021-03-02 08:57:00 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("input");
+        System.out.println("handle");
+        System.out.println("output");
+    }
+}
